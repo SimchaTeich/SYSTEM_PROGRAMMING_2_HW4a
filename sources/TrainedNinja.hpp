f9f22dfaebd541a37fe1d1b3ed1ced3067e44370
SimchaTeich <sim.teich@gmail.com>
2023-05-14 20:12:03 +0300

    define class TrainedNinja

---
+++ sources/TrainedNinja.hpp
@@ -0,0 +1,12 @@
+#ifndef TRAINEDNINJA_HPP
+#define TRAINEDNINJA_HPP
+
+#include "Ninja.hpp"
+
+class TrainedNinja : public Ninja
+{
+public:
+    TrainedNinja(const string& name, const Point& location);
+};
+
+#endif // !TRAINEDNINJA_HPP 

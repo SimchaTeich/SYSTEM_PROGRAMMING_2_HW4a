9cd79bec15103b5eb8977725426980d2318e2390
SimchaTeich <sim.teich@gmail.com>
2023-05-14 20:09:50 +0300

    define class YoungNinja

---
+++ sources/YoungNinja.hpp
@@ -0,0 +1,12 @@
+#ifndef YOUNGNINJA_HPP
+#define YOUNGNINJA_HPP
+
+#include "Ninja.hpp"
+
+class YoungNinja : public Ninja
+{
+public:
+    YoungNinja(const string& name, const Point& location);
+};
+
+#endif // !YOUNGNINJA_HPP 

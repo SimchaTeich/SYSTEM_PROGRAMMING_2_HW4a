8d37209f7392163be0cb5f9ad6a2c53e2811acb5
SimchaTeich <sim.teich@gmail.com>
2023-05-14 20:51:20 +0300

    append namespace ariel

--- sources/Team.hpp
+++ sources/Team.hpp
@@ -10,42 +10,45 @@
 
 #define MAX_PLAYERS 10
 
-class Team
+namespace ariel
 {
-private:
-    Cowboy *_cowboys[MAX_PLAYERS];
-    Ninja *_ninjas[MAX_PLAYERS];
-    int _amountOfCowboys;
-    int _amountOfNinjas;
-    Character *_captain;
+    class Team
+    {
+    private:
+        Cowboy *_cowboys[MAX_PLAYERS];
+        Ninja *_ninjas[MAX_PLAYERS];
+        int _amountOfCowboys;
+        int _amountOfNinjas;
+        Character *_captain;
 
-public:
-    Team(Character *captain);
-    ~Team(); // need to define all destructors of all as virtual.
+    public:
+        Team(Character *captain);
+        ~Team(); // need to define all destructors of all as virtual.
 
-    /*
-    * append new player into the team
-    * by type, if we still have less then
-    * MAX_PLAYERS players.. else, raise exception.
-    */
-    void add(Character *player);
+        /*
+        * append new player into the team
+        * by type, if we still have less then
+        * MAX_PLAYERS players.. else, raise exception.
+        */
+        void add(Character *player);
 
-    /*
-    * full explanation in README.md
-    */
-    void attack(Team *enemies);
+        /*
+        * full explanation in README.md
+        */
+        void attack(Team *enemies);
 
-    /*
-    * Returns the number of players in
-    * the team that still alive..
-    */
-    int stillAlive() const;
+        /*
+        * Returns the number of players in
+        * the team that still alive..
+        */
+        int stillAlive() const;
 
-    /*
-    * print details about the players.
-    * full explanation in README.md
-    */
-    string print() const;
+        /*
+        * print details about the players.
+        * full explanation in README.md
+        */
+        string print() const;
+    };
 };
 
 #endif // !TEAM_HPP 

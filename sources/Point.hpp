49dfefae96d37e40a8512d9c11717d3126210841
SimchaTeich <sim.teich@gmail.com>
2023-05-14 19:07:17 +0300

    define class Point

---
+++ sources/Point.hpp
@@ -0,0 +1,41 @@
+#ifndef POINT_HPP
+#define POINT_HPP
+
+
+class Point
+{
+private:
+    double _x;
+    double _y;
+
+public:
+    Point(double x, double y);
+    Point(const Point& other);
+
+    // getters
+    int x() const;
+    int y() const;
+
+    /*
+    Calc distance between this Point
+    to other Point.
+    input: other Point
+    output: the distance type double.
+    */
+    double distance(const Point& other) const;
+
+    /*
+    print Point in this format:
+    "(x,y)"
+    */
+    void print() const;
+
+    /*
+    calc some Point on the line, define by src and dst,
+    with distance start from src.
+    */
+    static Point moveToWards(const Point& src, const Point& dst, double distance);
+};
+
+
+#endif // !POINT_HPP

2384e61858be5efbeeb091a283a859e9cdddd9b6
SimchaTeich <sim.teich@gmail.com>
2023-05-14 19:40:26 +0300

    define class Cowboy

---
+++ sources/Cowboy.hpp
@@ -0,0 +1,40 @@
+#ifndef COWBOY_HPP
+#define COWBOY_HPP
+
+#include "Character.hpp"
+
+
+class Cowboy : public Character
+{
+private:
+    int _bullets;
+
+public:
+    Cowboy(const string& name, const Point& location);
+
+    /*
+    * If we are alive, and if we have bullets left,
+    * we subtract 10 hit points from the enemy and lose one bullet.
+    * Otherwise, nothing will happen to the enemy.
+    */
+    void shoot(Character *other);
+
+    /*
+    * Returns true if we have at least one more bullet
+    */
+    bool hasBullets() const;
+    bool hasBoolets() const; // same as the abouv.
+                             // but unfortantly this is
+                             // a mistake of how created this
+                             // taks. I have to implement it
+                             // for the automatic testers...
+
+
+    /*
+    * Loads the gun with six new bullets.
+    */
+    void reload();
+};
+
+
+#endif // !COWBOY_HPP

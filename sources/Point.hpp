f5fff78a93fbee34c53eddd19f2ff26b93b560f1
SimchaTeich <sim.teich@gmail.com>
2023-05-14 20:50:23 +0300

    append namespace ariel

--- sources/Point.hpp
+++ sources/Point.hpp
@@ -1,40 +1,42 @@
 #ifndef POINT_HPP
 #define POINT_HPP
 
-
-class Point
+namespace ariel
 {
-private:
-    double _x;
-    double _y;
+    class Point
+    {
+    private:
+        double _x;
+        double _y;
 
-public:
-    Point(double x, double y);
-    Point(const Point& other);
+    public:
+        Point(double x, double y);
+        Point(const Point& other);
 
-    // getters
-    int x() const;
-    int y() const;
+        // getters
+        int x() const;
+        int y() const;
 
-    /*
-    Calc distance between this Point
-    to other Point.
-    input: other Point
-    output: the distance type double.
-    */
-    double distance(const Point& other) const;
+        /*
+        Calc distance between this Point
+        to other Point.
+        input: other Point
+        output: the distance type double.
+        */
+        double distance(const Point& other) const;
 
-    /*
-    print Point in this format:
-    "(x,y)"
-    */
-    void print() const;
+        /*
+        print Point in this format:
+        "(x,y)"
+        */
+        void print() const;
 
-    /*
-    calc some Point on the line, define by src and dst,
-    with distance start from src.
-    */
-    static Point moveToWards(const Point& src, const Point& dst, double distance);
+        /*
+        calc some Point on the line, define by src and dst,
+        with distance start from src.
+        */
+        static Point moveToWards(const Point& src, const Point& dst, double distance);
+    };
 };
 
 

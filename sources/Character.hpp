a8282047306477865f7b25256ddeedd606f34bd3
SimchaTeich <sim.teich@gmail.com>
2023-05-14 20:47:21 +0300

    append namespace ariel

--- sources/Character.hpp
+++ sources/Character.hpp
@@ -7,52 +7,56 @@
 
 using std::string;
 
-class Character
+
+namespace ariel
 {
-private:
-    string& _name;
-    Point& _location;
-    bool _inTeam;
+    class Character
+    {
+    private:
+        string& _name;
+        Point& _location;
+        bool _inTeam;
 
-protected:
-    int _hitPoints;
-    char _type; // 'C' or 'N'
+    protected:
+        int _hitPoints;
+        char _type; // 'C' or 'N'
 
-public:
-    Character(const string& name, const Point& location);
+    public:
+        Character(const string& name, const Point& location);
 
-    /*
-    * check if Character is alive, by checking if
-    * there is for at least one hit point was left.
-    */
-    bool isAlive() const;
-    
-    /*
-    * returns the distance between this
-    * Character to other Character.
-    */
-    double distance(Character *other) const;
-    
-    /*
-    * douwn number of points from hit points left.
-    */
-    void hit(const int& points);
+        /*
+        * check if Character is alive, by checking if
+        * there is for at least one hit point was left.
+        */
+        bool isAlive() const;
+        
+        /*
+        * returns the distance between this
+        * Character to other Character.
+        */
+        double distance(Character *other) const;
+        
+        /*
+        * douwn number of points from hit points left.
+        */
+        void hit(const int& points);
 
-    /* getters */
-    string getName() const;
-    Point getLocation() const;
-    char getType() const;
-    bool inTeam() const;
+        /* getters */
+        string getName() const;
+        Point getLocation() const;
+        char getType() const;
+        bool inTeam() const;
 
-    /* 
-    * prints some details about the Character.
-    */
-    string print() const;
+        /* 
+        * prints some details about the Character.
+        */
+        string print() const;
 
-    /* updates that player is now belongs to team.
-    *  actualy, just turn on the _inTeam flag.
-    */
-    void insertedToTeam();
+        /* updates that player is now belongs to team.
+        *  actualy, just turn on the _inTeam flag.
+        */
+        void insertedToTeam();
+    };
 };
 
 

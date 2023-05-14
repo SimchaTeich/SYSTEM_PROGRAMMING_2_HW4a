c479a1c134b7a46f063d13ed50b1e580d6c96278
SimchaTeich <sim.teich@gmail.com>
2023-05-14 20:13:31 +0300

    define class OldNinja

---
+++ sources/OldNinja.hpp
@@ -0,0 +1,12 @@
+#ifndef OLDNINJA_HPP
+#define OLDNINJA_HPP
+
+#include "Ninja.hpp"
+
+class OldNinja : public Ninja
+{
+public:
+    OldNinja(const string& name, const Point& location);
+};
+
+#endif // !OLDNINJA_HPP 

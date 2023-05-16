0f0831ddfe4c0e89151fb2d6dd65e97e3acdecb2
SimchaTeich <sim.teich@gmail.com>
2023-05-16 09:54:05 +0300

    implements class YoungNinja

--- sources/YoungNinja.hpp
+++ sources/YoungNinja.hpp
@@ -8,7 +8,7 @@ namespace ariel
     class YoungNinja : public Ninja
     {
     public:
-        YoungNinja(const string& name, const Point& location);
+        YoungNinja(const string& name, Point& location);
     };
 };
 

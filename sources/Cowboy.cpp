ed80a8a16a8bceaf7f7e3ee6d88cd217a64d2ec3
SimchaTeich <sim.teich@gmail.com>
2023-05-16 09:38:36 +0300

    implements class Cowboy

---
+++ sources/Cowboy.cpp
@@ -0,0 +1,38 @@
+#include "Cowboy.hpp"
+
+using namespace ariel;
+
+Cowboy::Cowboy(const string& name, Point& location)
+    : Character(name, location)
+{
+    _hitPoints = 110;
+    reload();
+}
+
+
+void Cowboy::shoot(Character *other)
+{
+    if(isAlive() && hasBullets())
+    {
+        other->hit(10);
+        _bullets--;
+    }
+}
+
+
+bool Cowboy::hasBullets() const
+{
+    return _bullets >= 1;
+}
+
+
+bool Cowboy::hasBoolets() const
+{
+    return hasBullets();
+}
+
+
+void Cowboy::reload()
+{
+    _bullets += 6;
+}
